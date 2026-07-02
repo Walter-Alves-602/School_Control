8f2221ffc167b82386032b2b73edb81dca396c58
aluno <rickwemercunha@gmail.com>
2026-07-02 19:07:39 -0300

    feacth criando a tela de login

--- src/Login.jsx
+++ src/Login.jsx
@@ -11,8 +11,15 @@ function TelaLogin() {
 
       <section id="formulario">
         <img src={BannerAvatar} alt="Banner Avatar" />
+
         <form action="" method="get">
-          <label htmlFor="">ad</label>
+          <h1>Bem Vindo de Volta !</h1>
+          <p>faça o login para acessar sua conta</p>
+          <label htmlFor="">email</label>
+          <input type="email" name="" id="emailid" />
+          <label htmlFor="">passoword</label>
+          <input type="password" name="" id="passoword" />
+          
         </form>
       </section>
     </>
